536021b6c5912f7ae6f54f7010b7f20d60045d79
Oriol Canalias <oriol.canalias@gmail.com>
2018-02-12 08:34:38 -0200

    Testing two api-servers

--- api-server/src/main/java/br/com/devcave/springcloud/eureka/apiserver/controller/ApiServerController.java
+++ api-server/src/main/java/br/com/devcave/springcloud/eureka/apiserver/controller/ApiServerController.java
@@ -1,13 +1,16 @@
 package br.com.devcave.springcloud.eureka.apiserver.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class ApiServerController {
 
     @GetMapping("/test")
     public String getTestValue(){
+        log.info("M=getTestValue");
         return "test-value";
     }
 }
